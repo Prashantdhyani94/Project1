3479c13f5b3be2a7ac4a5a0a08c611cf8042cc87
Prashant Dhyani <127848010+Prashantdhyani94@users.noreply.github.com>
2023-04-12 13:36:49 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.youtube.com/" target={"blank"}>Youtube</a>
+            <a href="https://www.facebook.com/prashant.dhyani.77" target={"blank"}>Facebook</a>
+            <a href="https://www.instagram.com/dhyaniprashant73/?hl=en" target={"blank"}>Instagram</a>
+
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
